cc92b0ff8904915a521f9962fe5281d20fbd92ea
Bálint Ducsai <balint.ducsai@gmail.com>
2026-05-16 15:01:03 +0200

    refactor(sidebar): replace placeholder icon with lucide square icon

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,33 +1,19 @@
 import { Component } from "solid-js";
+import { Square } from "lucide-solid";
 import SidebarItem from "./SidebarItem";
-
-const PlaceholderIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    stroke-linejoin="round"
-    stroke-linecap="round"
-    stroke-width="2"
-    fill="none"
-    stroke="currentColor"
-    class="size-5"
-  >
-    <rect x="3" y="3" width="18" height="18" rx="2" />
-  </svg>
-);
 
 const Sidebar: Component = () => {
   return (
     <aside class="min-h-screen w-16 items-center bg-base-200 flex flex-col justify-between py-4">
       <ul class="d-menu gap-2">
-        <SidebarItem href="/" icon={PlaceholderIcon} label="Stage" />
-        <SidebarItem href="/revise" icon={PlaceholderIcon} label="Revise" />
-        <SidebarItem href="/stats" icon={PlaceholderIcon} label="Stats" />
-        <SidebarItem href="/goals" icon={PlaceholderIcon} label="Goals" />
+        <SidebarItem href="/" icon={() => <Square class="size-5" />} label="Stage" />
+        <SidebarItem href="/revise" icon={() => <Square class="size-5" />} label="Revise" />
+        <SidebarItem href="/stats" icon={() => <Square class="size-5" />} label="Stats" />
+        <SidebarItem href="/goals" icon={() => <Square class="size-5" />} label="Goals" />
       </ul>
       <ul class="d-menu gap-2">
-        <SidebarItem href="/settings" icon={PlaceholderIcon} label="Settings" />
-        <SidebarItem href="/profile" icon={PlaceholderIcon} label="Profile" />
+        <SidebarItem href="/settings" icon={() => <Square class="size-5" />} label="Settings" />
+        <SidebarItem href="/profile" icon={() => <Square class="size-5" />} label="Profile" />
       </ul>
     </aside>
   );
